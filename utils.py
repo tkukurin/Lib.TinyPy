a90aff58e62dc7af4501064af6f9ea7bae1b1fc6
Toni Kukurin <tkukurin@gmail.com>
2020-02-20 18:54:46 +0100

    add module loader

--- utils.py
+++ utils.py
@@ -2,18 +2,52 @@
 import functools as ft
 import itertools as it
 import operator
+import typing
+import types
 
 from typing import (
-  Union, List, Callable, Iterable)
+  Union, List, Callable, Iterable, Iterator,)
 
-from . import funct
+import funct
 
 
 idx = operator.itemgetter
 partial = ft.partial
+nt = typing.NamedTuple
+const = types.SimpleNamespace
 
 # Take a look at an element from iterable
 peek = funct.compose(next, iter)
+
+
+class ModLoad:
+  ''' Helper to load from another Python project.
+  Really shouldn't be used, but sometimes useful for Jupyter prototyping.
+
+  ex: ModLoad('/home/toni/PythonProj')
+  '''
+
+  def __init__(self, where):
+    from pathlib import Path
+    self.where = Path(
+      where.where if isinstance(where, ModLoad)
+      else where)
+
+  def __call__(self, modname):
+    '''Load module `modname` from `self.where/modname.py`'''
+    import importlib.util
+    modname = modname.strip('.py')
+    spec = importlib.util.spec_from_file_location(
+      modname, self.where / f'{modname}.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+  def make(pathobj: const):
+    for name, val in pathobj.__dict__.items():
+      if name != 'base':
+        setattr(pathobj, name, ModLoad(val))
+    return pathobj
 
 
 def flatmap(func: Callable, iterable: Iterable):
