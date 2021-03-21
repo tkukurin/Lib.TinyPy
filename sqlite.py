c44a33f8b006254ebd0d9fdbc755dd0e3f43a299
Toni Kukurin <tkukurin@gmail.com>
2021-03-21 12:06:15 +0100

    sqlite use row factory

--- sqlite.py
+++ sqlite.py
@@ -1,9 +1,22 @@
 '''Simple query from Sqlite3 databases.
+
+Usage:
+  with Fetcher('db.sqlite') as f:
+    table = f.for_('table')
+    single_row = table.qone('id', 'name')
+
+Usage:
+  fetcher = Fetcher('db.sqlite')
+  with fetcher.for_('table') as table:
+    single_row = table.qone('id', 'name')
 '''
 
 import sqlite3
 import re
 import functools
+import contextlib
+import typing as T
+import logging
 
 from dataclasses import dataclass, field
 from types import SimpleNamespace as ns
@@ -12,17 +25,31 @@ from collections import namedtuple
 from pathlib import Path
 
 
+L = logging.getLogger(__name__)
+
+
 def _sql_cleanup(sql: str):
-  # remove CREATE TABLE and get rid of extra whitespace
-  # TODO(tk) probably should handle other types of table definitions too
-  return [
-    re.sub(' +', ' ', line.strip(' ,'))
-    for line in sql.split('\n')[1:-1]
-  ]
+  def _structure(line):
+    name, type_, *meta = re.split('\\s+', line.strip(' ,'))
+
+    if type_ == 'integer': type_ = int
+    elif type_ == 'real': type_ = float
+    elif type_ == 'text': type_ = str
+    elif type_ == 'blob': type_ = bytes
+    else: type_ = None
+
+    return ns(
+      name=name, type=type_, meta=' '.join(meta))
+
+  # 1st and last lines are not named columns (`create ...` and `)`)
+  # TODO(tk) check if true for every db?
+  return [_structure(line) for line in sql.split('\n')[1:-1]]
 
 
-@dataclass
 class Fetcher:
+  def __init__(self, database):
+    self.database = database
+    self.conn = None
 
   @dataclass
   class TableInfo:
@@ -37,32 +64,36 @@ class Fetcher:
       '''SELECT tbl_name, sql FROM sqlite_master WHERE type='table' ''')
     return [self.TableInfo(*t) for t in tables]
 
-  def __init__(self, database: Union[str, Path]):
-    '''Bind fetcher to a database file.
-    NOTE: creates a single persistent connection.
-    '''
-    self.conn = sqlite3.connect(database)
+  def __enter__(self):
+    if not self.conn:
+      self.conn = sqlite3.connect(self.database)
+      self.conn.row_factory = sqlite3.Row
+    return self
 
-  def __del__(self):
-    self.conn.close()
+  def __exit__(self, exc_type, exc_value, exc_tb):
+    if self.conn:
+      if exc_type is None:
+        self.conn.commit()
+      else:
+        L.exception('Rolling back transaction')
+        self.conn.rollback()
+      self.conn.close()
+    self.conn = None
 
-  for_ = lambda self, table: TableFetcher(self, table)
+  def for_(self, table: T.Union[str, TableInfo]):
+    if isinstance(table, self.TableInfo):
+      table = table.name
+    return TableFetcher(self, table)
 
   exe = lambda self, *a: self.conn.execute(' '.join(a))
-  qone = lambda self, *a, **kw: self.exe(*a, **kw).fetchone()
-  qall = lambda self, *a, **kw: self.exe(*a, **kw).fetchall()
-
-
-def _select_cleanup(select):
-  # make sure namespace doesn't contain brackets in queries below
-  return re.sub(r'[\(|\)]', '', select)
-
-def _zipns(keys, vals):
-  return ns(**dict(zip(keys, vals)))
+  qone = lambda self, *a: self.exe(*a).fetchone()
+  qall = lambda self, *a: self.exe(*a).fetchall()
 
 
 @dataclass
 class TableFetcher:
+  '''Can be used either as a context mgr or within fetcher's context mgr.
+  '''
   fetcher: Fetcher
   table: str
 
@@ -72,38 +103,28 @@ class TableFetcher:
       f'SELECT sql FROM sqlite_master WHERE name=\'{self.table}\'')[0]
     return _sql_cleanup(sql)
 
-  qq = lambda self, sel, *a: self.fetcher.exe(
-    f'SELECT {sel} FROM {self.table} ' + ' '.join(a))
+  def __enter__(self):
+    self.fetcher.__enter__()
+    return self
 
-  # suitable for * queries
-  qone = lambda self, sel, *a: self.qq(sel, *a).fetchone()
-  qall = lambda self, sel, *a: self.qq(sel, *a).fetchall()
+  def __exit__(self, *a):
+    self.fetcher.__exit__(*a)
 
-  def qone_(self, *selects: str, **kw):
-    cursor, selects = self._get_named(*selects, **kw)
-    return _zipns(selects, cursor.fetchone())
+  def qone(self, *sel, **kw):
+    return ns(**self.qd(*sel, **kw).fetchone())
 
-  def qmany_(self, *selects: str, size=10, **kw):
-    cursor, selects = self._get_named(*selects, **kw)
-    while rows := cursor.fetchmany(size=size):
-      # Yield in batches
-      yield map(lambda row: _zipns(selects, row), rows)
+  def qgen(self, *sel, **kw):
+    return map(lambda row: ns(**row), self.qd(*sel, **kw))
 
-  def qall_(self, *selects: str, **kw):
-    return list(self.qgen_(*selects, **kw))
-
-  def qgen_(self, *selects: str, **kw):
-    cursor, selects = self._get_named(*selects, **kw)
-    yield map(lambda row: _zipns(selects, row), rows)
-
-  def _get_named(self, *selects, **kw):
-    data = self.qd(*selects, **kw)
-    selects = list(map(_select_cleanup, selects))
-    return data, selects
+  def qall(self, *sel, **kw):
+    return list(self.qgen(*sel, **kw))
 
   def qd(self, *sel, **kw):
     '''Ex: qd('title', WHERE='title <> "abc"', LIMIT=5)
     '''
     query_params = ' '.join(f'{k} {v}' for k, v in kw.items())
     return self.qq(','.join(sel), query_params)
+
+  qq = lambda self, sel, *a: self.fetcher.exe(
+    f'SELECT {sel} FROM {self.table} ' + ' '.join(a))
 
