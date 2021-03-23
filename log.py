da1fbeae859edf297b6c90cee1b596c95b5538a3
Toni Kukurin <tkukurin@gmail.com>
2021-03-23 11:11:43 +0100

    update log

--- log.py
+++ log.py
@@ -1,24 +1,23 @@
+import typing as T
+
 import logging
 import sys
 import io
 
+from pathlib import Path
 from functools import wraps
 
 
-cfg = lambda: logging.basicConfig(**_getcfg())
+def cfg(name=None, level=logging.DEBUG):
+  logging.basicConfig(**_getcfg(level=level))
+  return logging.getLogger(name)
 
 
-def _getcfg():
+def _getcfg(level=logging.DEBUG):
   return dict(
-    format='[%(levelname)s] %(asctime)s %(message)s',
-    datefmt='%Y-%m-%d %H:%M',
-    level=logging.DEBUG)
-
-
-def _streamhandler(stream, formatter):
-  sh = logging.StreamHandler(stream)
-  sh.formatter = formatter
-  return sh
+      format='[%(levelname)s|%(module)s:%(lineno)s|%(asctime)s] %(message)s',
+    datefmt='%y-%m-%d %X',
+    level=level)
 
 
 def get_strlog(name, stream=None, fmt=None, datefmt=None):
@@ -36,10 +35,13 @@ class LogPrinter:
     self.log.setLevel(logging.DEBUG)
 
   def write(self, text):
-    self.log.debug(text)
+    if text.strip():
+      self.log.debug(text)
 
 
 def printlog(func):
+  '''Treat print as log within some function. Assumes single-thread execution.
+  '''
   @wraps(func)
   def pwrapper(*arg, **kwargs):
     stdold = sys.stdout
@@ -49,20 +51,32 @@ def printlog(func):
     finally: sys.stdout = stdold
   return pwrapper
 
-def logto(stream, formatter=None):
+
+def to(stream_or_fname: T.Union[io.IOBase, str, Path]):
+  '''Temp redirect log. Assumes single-thread execution.
+  If stream_or_fname is path, will open/close file as appropriate.
+  '''
   logger = logging.getLogger()
+  is_fname = isinstance(stream_or_fname, (str, Path))
+
   def _inner(func):
     @wraps(func)
     def _innerinner(*a, **kw):
       hs_old = logger.handlers
+      stream = open(stream_or_fname, 'a') if is_fname else stream_or_fname
       try:
-        formatter = (formatter or (
-          hs_old[-1].formatter if hs_old else logging.Formatter()))
+        formatter = hs_old[-1].formatter if hs_old else logging.Formatter()
         logger.handlers = [_streamhandler(stream, formatter)]
         return func(*a, **kw)
-      finally: logger.handlers = hs_old
+      finally:
+        logger.handlers = hs_old
+        if is_fname: stream.close()
     return _innerinner
   return _inner
 
 
+def _streamhandler(stream, formatter):
+  sh = logging.StreamHandler(stream)
+  sh.formatter = formatter
+  return sh
 
